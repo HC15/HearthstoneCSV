bfcde07bc3743ee2992fa4de4ed20780047a4dbe
Harvey Chen <harvc015@gmail.com>
2016-06-03 06:18:02 -0400

    Updated file to also include card mechanics

--- heathstone.py
+++ heathstone.py
@@ -11,32 +11,57 @@ wildSet = ["Naxxramas", "Goblins vs Gnomes", "Reward", "Promotion"]
 allSet = standardSet + wildSet
 
 with open('hearthstone.csv', 'w', newline = '') as csvfile:
-    fieldNames = ["Card Name", "Card Set", "Card Type", "Class", "Rarity", "Mana Cost", "Attack", "Health", "Race"]
+    fieldNames = ["Card Name", "Card Set", "Card Type", "Class", "Rarity",
+                  "Mana Cost", "Attack", "Health", "Card Mechanics", "Race"]
     writer = csv.DictWriter(csvfile, fieldnames=fieldNames)
     writer.writeheader()
 
     for cardSet in allSet:
         cardSetInfo = cardInfo[cardSet]
+
         for card in cardSetInfo:
+            cardMechanicsList = []
+            if card.get("mechanics") is not None:
+                cardMechanicsList = card.get("mechanics")
+
+            cardMechanicsStr = ""
+            for i in range(len(cardMechanicsList)):
+                cardMechanic = cardMechanicsList[i]["name"]
+                if cardMechanic == "ImmuneToSpellpower":
+                    cardMechanic = "Immune to Spell Power"
+                elif cardMechanic == "HealTarget":
+                    cardMechanic = "Heal Target"
+                elif cardMechanic == "AdjacentBuff":
+                    cardMechanic = "Adjacent Buff"
+                elif cardMechanic == "AffectedBySpellPower":
+                    cardMechanic = "Affected by Spell Power"
+
+                if i == 0:
+                    cardMechanicsStr += cardMechanic
+                else:
+                    cardMechanicsStr += ", " + cardMechanic
+
             if card["type"] == "Minion":
                 if "playerClass" in card:
                     writer.writerow({"Card Name": card.get("name"), "Card Set": card.get("cardSet"),
                                      "Card Type": card.get("type"), "Class": card.get("playerClass"),
                                      "Rarity": card.get("rarity"), "Mana Cost": card.get("cost"),
                                      "Attack": str(card.get("attack")), "Health": str(card.get("health")),
-                                     "Race": card.get("race")})
+                                     "Card Mechanics": cardMechanicsStr, "Race": card.get("race")})
                 else:
                     writer.writerow({"Card Name": card.get("name"), "Card Set": card.get("cardSet"),
                                      "Card Type": card.get("type"), "Class": "Neutral",
                                      "Rarity": card.get("rarity"), "Mana Cost": card.get("cost"),
                                      "Attack": str(card.get("attack")), "Health": str(card.get("health")),
-                                     "Race": card.get("race")})
+                                     "Card Mechanics": cardMechanicsStr, "Race": card.get("race")})
             if card["type"] == "Spell":
                 writer.writerow({"Card Name": card.get("name"), "Card Set": card.get("cardSet"),
                                  "Card Type": card.get("type"), "Class": card.get("playerClass"),
-                                 "Rarity": card.get("rarity"), "Mana Cost": card.get("cost")})
+                                 "Rarity": card.get("rarity"), "Mana Cost": card.get("cost"),
+                                 "Card Mechanics": cardMechanicsStr})
             if card["type"] == "Weapon":
                 writer.writerow({"Card Name": card.get("name"), "Card Set": card.get("cardSet"),
                                  "Card Type": card.get("type"), "Class": card.get("playerClass"),
                                  "Rarity": card.get("rarity"), "Mana Cost": card.get("cost"),
-                                 "Attack": str(card.get("attack")), "Health": str(card.get("durability"))})
+                                 "Attack": str(card.get("attack")), "Health": str(card.get("durability")),
+                                 "Card Mechanics": cardMechanicsStr})
